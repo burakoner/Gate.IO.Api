0f4e005f6cc6607cbc16c0b7d44bdd09476b0ffb
Burak Öner <info@burakoner.com>
2023-03-07 22:10:47 +0300

    Version 1.1.5

--- src/Gate.IO.Api/GateRestApiClientOptions.cs
+++ src/Gate.IO.Api/GateRestApiClientOptions.cs
@@ -9,9 +9,6 @@ public class GateRestApiClientOptions : RestApiClientOptions
     public bool AutoTimestamp { get; set; }
     public TimeSpan TimestampRecalculationInterval { get; set; }
 
-    // Broker Id
-    public string BrokerId { get; set; }
-
     public GateRestApiClientOptions() : this(null)
     {
     }
@@ -23,59 +20,6 @@ public class GateRestApiClientOptions : RestApiClientOptions
 
         // Api Addresses
         BaseAddress = GateApiAddresses.Default.RestApiAddress;
-
-        // Rate Limiters
-        RateLimiters = new List<IRateLimiter>
-        {
-            new RateLimiter()
-                // General
-                .AddTotalRateLimit(900, TimeSpan.FromSeconds(1))
-
-                // Spot
-                .AddEndpointLimit("/api/v4/spot/orders", 10, TimeSpan.FromSeconds(1), HttpMethod.Post)
-                .AddEndpointLimit("/api/v4/spot/orders", 5000, TimeSpan.FromSeconds(1), HttpMethod.Delete)
-                .AddEndpointLimit("/api/v4/spot/batch_orders", 10, TimeSpan.FromSeconds(1), HttpMethod.Post)
-                .AddEndpointLimit("/api/v4/spot/batch_orders", 5000, TimeSpan.FromSeconds(1), HttpMethod.Delete)
-                
-                // Perpetual 
-                .AddPartialEndpointLimit("/futures/", 300, TimeSpan.FromSeconds(1), null)
-                .AddEndpointLimit("/api/v4/futures/btc/orders", 100, TimeSpan.FromSeconds(1), HttpMethod.Post)
-                .AddEndpointLimit("/api/v4/futures/btc/batch_orders", 100, TimeSpan.FromSeconds(1), HttpMethod.Post)
-                .AddEndpointLimit("/api/v4/futures/btc/orders", 400, TimeSpan.FromSeconds(1), HttpMethod.Delete)
-                .AddEndpointLimit("/api/v4/futures/btc/batch_orders", 400, TimeSpan.FromSeconds(1), HttpMethod.Delete)
-                .AddEndpointLimit("/api/v4/futures/usd/orders", 100, TimeSpan.FromSeconds(1), HttpMethod.Post)
-                .AddEndpointLimit("/api/v4/futures/usd/batch_orders", 100, TimeSpan.FromSeconds(1), HttpMethod.Post)
-                .AddEndpointLimit("/api/v4/futures/usd/orders", 400, TimeSpan.FromSeconds(1), HttpMethod.Delete)
-                .AddEndpointLimit("/api/v4/futures/usd/batch_orders", 400, TimeSpan.FromSeconds(1), HttpMethod.Delete)
-                .AddEndpointLimit("/api/v4/futures/usdt/orders", 100, TimeSpan.FromSeconds(1), HttpMethod.Post)
-                .AddEndpointLimit("/api/v4/futures/usdt/batch_orders", 100, TimeSpan.FromSeconds(1), HttpMethod.Post)
-                .AddEndpointLimit("/api/v4/futures/usdt/orders", 400, TimeSpan.FromSeconds(1), HttpMethod.Delete)
-                .AddEndpointLimit("/api/v4/futures/usdt/batch_orders", 400, TimeSpan.FromSeconds(1), HttpMethod.Delete)
-
-                // Delivery 
-                .AddPartialEndpointLimit("/delivery/", 300, TimeSpan.FromSeconds(1), null)
-                .AddEndpointLimit("/api/v4/delivery/btc/orders", 100, TimeSpan.FromSeconds(1), HttpMethod.Post)
-                .AddEndpointLimit("/api/v4/delivery/btc/batch_orders", 100, TimeSpan.FromSeconds(1), HttpMethod.Post)
-                .AddEndpointLimit("/api/v4/delivery/btc/orders", 400, TimeSpan.FromSeconds(1), HttpMethod.Delete)
-                .AddEndpointLimit("/api/v4/delivery/btc/batch_orders", 400, TimeSpan.FromSeconds(1), HttpMethod.Delete)
-                .AddEndpointLimit("/api/v4/delivery/usdt/orders", 100, TimeSpan.FromSeconds(1), HttpMethod.Post)
-                .AddEndpointLimit("/api/v4/delivery/usdt/batch_orders", 100, TimeSpan.FromSeconds(1), HttpMethod.Post)
-                .AddEndpointLimit("/api/v4/delivery/usdt/orders", 400, TimeSpan.FromSeconds(1), HttpMethod.Delete)
-                .AddEndpointLimit("/api/v4/delivery/usdt/batch_orders", 400, TimeSpan.FromSeconds(1), HttpMethod.Delete)
-
-                // Swap 
-                .AddPartialEndpointLimit("/flash_swap/", 300, TimeSpan.FromSeconds(1), null)
-                .AddEndpointLimit("/api/v4/flash_swap/orders", 100, TimeSpan.FromSeconds(1), HttpMethod.Post)
-                .AddEndpointLimit("/api/v4/flash_swap/batch_orders", 100, TimeSpan.FromSeconds(1), HttpMethod.Post)
-                .AddEndpointLimit("/api/v4/flash_swap/orders", 400, TimeSpan.FromSeconds(1), HttpMethod.Delete)
-                .AddEndpointLimit("/api/v4/flash_swap/batch_orders", 400, TimeSpan.FromSeconds(1), HttpMethod.Delete)
-                
-                // Wallet 
-                .AddPartialEndpointLimit("/wallet/", 200, TimeSpan.FromSeconds(1), null)
-
-                // Withdrawal 
-                .AddEndpointLimit("/api/v4/withdrawals", 1, TimeSpan.FromSeconds(3), HttpMethod.Post)
-        };
 
         // Receive Window
         ReceiveWindow = TimeSpan.FromSeconds(5);
